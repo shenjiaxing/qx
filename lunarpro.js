090c7c6f6e80222040ddfbd53c60bebaad9ceb10
Jiaxing <jxshen.polyu@gmail.com>
2024-08-08 16:15:19 +0800

    Update lunarpro.js

--- lunarpro.js
+++ lunarpro.js
@@ -1,9 +1,31 @@
 var ojbk={
-    "code" : -1,
-    "_code" : 200,
-    "errorcode" : "-1"
-
-    };
+  "status" : "success",
+  "subscription" : {
+    "autorenew" : false,
+    "expiration" : "2099-10-10T06:38:23.000000Z"
+  },
+  "scope" : [
+    "rsvp",
+    "scheduling",
+    "openings",
+    "keyvalue-sharedtoken",
+    "keyvalue",
+    "keyvalue-verification",
+    "notify",
+    "location",
+    "keyvalue-watch",
+    "account",
+    "weather",
+    "schedjoules",
+    "fantastical",
+    "sso-connect",
+    "cardhop"
+  ],
+  "email" : "jxshen.polyu@gmail.com",
+  "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImU0YmY2ZDc4LTIyM2QtNDg0MS1iOWRkLWFhNThmZjlkMzc3ZCIsImVtYWlsIjoianhzaGVuLnBvbHl1QGdtYWlsLmNvbSIsImlhdCI6MTcyMzEwNDU4MSwiZXhwIjoxNzIzMTA4MTgxLCJkZXZpY2UiOiJtYWMtNkU0Mzk1OTYwMjQzNzgxQjVBMDk5QTU4MTczRDg5QUU3QUY4MzQyNTAzMERDMUVCNzQyMDVDMjc0NzM1MjU3RiIsInNjb3BlIjpbInJzdnAiLCJzY2hlZHVsaW5nIiwib3BlbmluZ3MiLCJrZXl2YWx1ZS1zaGFyZWR0b2tlbiIsImtleXZhbHVlIiwia2V5dmFsdWUtdmVyaWZpY2F0aW9uIiwibm90aWZ5IiwibG9jYXRpb24iLCJzY2hlZGpvdWxlcyIsImtleXZhbHVlLXdhdGNoIiwiYWNjb3VudCIsImZhbnRhc3RpY2FsIiwic3NvLWNvbm5lY3QiLCJ3ZWF0aGVyIiwiY2FyZGhvcCJdLCJhdWQiOiJmbGV4aWJpdHMvYXBpIiwiaXNzIjoiZmxleGliaXRzL2FwaSJ9.DSJ57DFEtIjp2pJRV7g_BEx5VUgWt243SiIc1WGeYL4",
+  "name" : "Jiaxing SHEN",
+  "user" : "e4bf6d78-223d-4841-b9dd-aa58ff9d377d"
+};
 
 
 
