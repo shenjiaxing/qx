828955195f2ecde2913ad4d85bda2d8632398108
Jiaxing <jxshen.polyu@gmail.com>
2024-08-08 15:22:04 +0800

    Update lunarpro.js

--- lunarpro.js
+++ lunarpro.js
@@ -1,12 +1,5 @@
 
-if ($response && $response.body) {
-    // Your code that manipulates $response.body
-    let obj = JSON.parse($response.body);
-	obj['success'] = true;
-
-	$done({body: JSON.stringify(obj)});
-} else {
-    console.log("【e】No response or response body found");
-}
+var chxm1023 = JSON.parse($response.body);
+console.log(chxm1023);
 
 
