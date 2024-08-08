e908773df90c0f2f27ed8f94a989704d3f47ed51
Jiaxing <jxshen.polyu@gmail.com>
2024-08-08 16:20:25 +0800

    Update lunarpro.js

--- lunarpro.js
+++ lunarpro.js
@@ -2,7 +2,7 @@ var ojbk={
   "status" : "success",
   "subscription" : {
     "autorenew" : false,
-    "expiration" : "2024-08-22T06:38:23.000000Z"
+    "expiration" : "2099-08-22T06:38:23.000000Z"
   },
   "scope" : [
     "rsvp",
