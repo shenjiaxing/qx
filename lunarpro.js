ac8acd7f24dd008e8dccf8952abf67d5779a4f5a
Jiaxing <jxshen.polyu@gmail.com>
2024-08-08 16:39:16 +0800

    Update lunarpro.js

--- lunarpro.js
+++ lunarpro.js
@@ -1,55 +1,103 @@
-// var ojbk={
-//   "status" : "success",
-//   "subscription" : {
-//     "autorenew" : false,
-//     "expiration" : "2099-08-22T06:38:23.000000Z"
-//   },
-//   "scope" : [
-//     "rsvp",
-//     "scheduling",
-//     "openings",
-//     "keyvalue-sharedtoken",
-//     "keyvalue",
-//     "keyvalue-verification",
-//     "notify",
-//     "location",
-//     "keyvalue-watch",
-//     "account",
-//     "weather",
-//     "schedjoules",
-//     "fantastical",
-//     "sso-connect",
-//     "cardhop"
-//   ],
-//   "email" : "jxshen.polyu@gmail.com",
-//   "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImU0YmY2ZDc4LTIyM2QtNDg0MS1iOWRkLWFhNThmZjlkMzc3ZCIsImVtYWlsIjoianhzaGVuLnBvbHl1QGdtYWlsLmNvbSIsImlhdCI6MTcyMzEwNDU4MSwiZXhwIjoxNzIzMTA4MTgxLCJkZXZpY2UiOiJtYWMtNkU0Mzk1OTYwMjQzNzgxQjVBMDk5QTU4MTczRDg5QUU3QUY4MzQyNTAzMERDMUVCNzQyMDVDMjc0NzM1MjU3RiIsInNjb3BlIjpbInJzdnAiLCJzY2hlZHVsaW5nIiwib3BlbmluZ3MiLCJrZXl2YWx1ZS1zaGFyZWR0b2tlbiIsImtleXZhbHVlIiwia2V5dmFsdWUtdmVyaWZpY2F0aW9uIiwibm90aWZ5IiwibG9jYXRpb24iLCJzY2hlZGpvdWxlcyIsImtleXZhbHVlLXdhdGNoIiwiYWNjb3VudCIsImZhbnRhc3RpY2FsIiwic3NvLWNvbm5lY3QiLCJ3ZWF0aGVyIiwiY2FyZGhvcCJdLCJhdWQiOiJmbGV4aWJpdHMvYXBpIiwiaXNzIjoiZmxleGliaXRzL2FwaSJ9.DSJ57DFEtIjp2pJRV7g_BEx5VUgWt243SiIc1WGeYL4",
-//   "name" : "Jiaxing SHEN",
-//   "user" : "e4bf6d78-223d-4841-b9dd-aa58ff9d377d"
-// };
+var ojbk=
+{
+  "uuid" : "e4bf6d78-223d-4841-b9dd-aa58ff9d377d",
+  "auth_types" : [
+    {
+      "email" : "jxshen.polyu@gmail.com",
+      "auth_type" : "google-oauth2"
+    }
+  ],
+  "ab_testing_cohort" : 1,
+  "devices" : [
+    {
+      "scheduling_version" : 2,
+      "cardhop_tier" : null,
+      "scope" : [
+        "fantastical",
+        "account"
+      ],
+      "model" : 1,
+      "uuid" : "8341ec4f-912a-4721-b7b5-d19c817cb772",
+      "identifier" : "ios-46F3EE24-B361-4BE4-8625-248AE9114268",
+      "last_active_at" : "2024-08-08T04:42:57.690423Z",
+      "fantastical2_tier" : null,
+      "openings_version" : 3,
+      "name" : "iPhone"
+    },
+    {
+      "scheduling_version" : 2,
+      "cardhop_tier" : null,
+      "scope" : [
+        "fantastical",
+        "account"
+      ],
+      "model" : 1,
+      "uuid" : "1ca6930a-3c33-4152-b2b9-fde8b1a0bbe6",
+      "identifier" : "ios-FA7588BC-3594-48AF-A392-9EB6A15E5D86",
+      "last_active_at" : "2024-05-29T23:40:40.893633Z",
+      "fantastical2_tier" : null,
+      "openings_version" : 3,
+      "name" : "iPhone"
+    },
+    {
+      "scheduling_version" : 2,
+      "cardhop_tier" : null,
+      "scope" : [
+        "fantastical",
+        "account"
+      ],
+      "model" : 0,
+      "uuid" : "6cc926c1-1ad6-4e79-bcc7-98091acfe5ba",
+      "identifier" : "mac-6E4395960243781B5A099A58173D89AE7AF83425030DC1EB74205C274735257F",
+      "last_active_at" : "2024-08-08T08:34:07.529664Z",
+      "fantastical2_tier" : null,
+      "openings_version" : 3,
+      "name" : "MacBook Pro"
+    }
+  ],
+  "repeat_trial_available" : false,
+  "has_dpa" : false,
+  "settings" : {
+    "locale" : "en",
+    "hour_cycle" : "h23",
+    "analytics_enabled" : false,
+    "week_starts_on" : 0
+  },
+  "team" : {
+    "kind" : "individual",
+    "uuid" : "f373a4ed-baa1-4482-99fe-cae9086ae00c"
+  },
+  "is_premium" : true,
+  "email_addresses" : [
+    {
+      "mailing_lists" : [
+
+      ],
+      "primary" : true,
+      "uuid" : "e0aea90a-afd6-4646-abc1-8c42ab16cb1e",
+      "email" : "jxshen.polyu@gmail.com",
+      "updated_at" : "2020-03-15T15:47:21.921737Z",
+      "verified_at" : "2020-03-15T15:47:21.921518Z",
+      "primary_proposal" : true
+    }
+  ],
+  "subscription" : {
+    "is_canceled" : false,
+    "autorenew" : false,
+    "started_at" : "2024-08-08T06:38:23.000000Z",
+    "trial" : false,
+    "subscription_type" : "AppStore",
+    "invoice_status" : "reconciled",
+    "uuid" : "1c9caab9-6f6a-4e39-9224-8b5316ad62a7",
+    "is_expired" : false,
+    "expiration" : "2026-08-22T06:38:23.000000Z",
+    "interval" : 0
+  },
+  "role" : "owner",
+  "username" : "jxshenpolyu-GMdp",
+  "openings_enabled" : true,
+  "name" : "Jiaxing SHEN"
+};
 
 
-// $done({body:JSON.stringify(ojbk)});
-
-
-let url = $request.url;
-const path1="v1/auth/device";
-const path2="v1/account/details";
-let obj = JSON.parse($response.body);
-if (url.indexOf(path1) != -1) {
-obj["subscription"]={
-  "autorenew": true,
-  "expiration": "2099-02-15T09:09:14Z",
-  "expires": "2099-02-15T09:09:14Z"
- }
-}
-if (url.indexOf(path2) != -1) {
-obj["subscription"]={
-  "autorenew": true,
-  "expiration": "2099-02-15T09:09:14.000000Z",
-  "uuid": "3c5927a6-4e3e-4773-b7af-62506e036dc7",
-  "subscription_type": "AppStore",
-  "is_expired": false,
-  "trial": false
- }
-}
-$done({body: JSON.stringify(obj)});
+$done({body:JSON.stringify(ojbk)});
